741341e8e04f824e063f34267d7e67b606b5063b
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-09-03 22:10:24 +0530

    nav-tabs added

---
+++ StockWidget/StockWidget/js/StockIntialize.js
@@ -0,0 +1,3 @@
+﻿document.write('<link href="css/bootstrap.min.css" rel="stylesheet" />');
+document.write('<link href="css/bootstrap-theme.min.css" rel="stylesheet" />');
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>');
---
+++ StockWidget/StockWidget/js/StockWidget.js
@@ -0,0 +1,55 @@
+﻿var wHtml = "";
+
+//---Tabs----
+    wHtml+=('<Div id="tab-bars">');
+    wHtml+=('<ul class="nav nav-tabs">');
+    wHtml += ('<li class="active"><a href="#sensex">sensex</a></li>');
+    wHtml += ('<li><a  href="#nifty">nifty</a></li>');
+    wHtml += ('<li><a href="#cac">cac</a></li>');
+    wHtml += ('<li ><a href="#dowjones">dowjones</a></li>');
+    wHtml += ('<li><a href="#ftse">ftse</a></li>');
+    wHtml += ('<li><a href="#nasdaq">nasdaq</a></li>');
+    wHtml += ('<li><a href="#nikkie225">nikkie225</a></li>');
+    wHtml += ('</ul>');
+    wHtml += ('</Div>');
+    wHtml += ('&nbsp');
+    wHtml += ('&nbsp');
+
+//---SENSEX result
+    wHtml += ('<Div id="result">');
+    wHtml += ('	<label>SENSEX</label>');
+    wHtml += ('</Div>');
+    wHtml += ('&nbsp');
+    wHtml += ('&nbsp');
+
+//---Range
+    wHtml += ('<Div id="range">');
+    wHtml += ('	<label>Todays HIGH/LOW</label>');
+    wHtml += ('</Div>');
+    wHtml += ('&nbsp');
+    wHtml += ('&nbsp');
+
+//---graph
+    wHtml += ('<Div id="graph">');
+    wHtml += ('	<label>Graph</label>');
+    wHtml += ('</Div>');
+    document.write(wHtml);
+
+//---tab clicked
+    $(document).ready(function () {
+        $(".nav-tabs a").click(function () {
+            var tabUrl = "" + this;
+            var tab = parseTab(tabUrl);
+            alert(tab);
+        })
+    });
+
+    var parseTab = function (tabUrl)
+    {
+        var tab = tabUrl.split('#');
+        return tab[1];
+    }
+    
+
+
+

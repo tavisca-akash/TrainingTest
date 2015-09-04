2f4569dab015667e356e991bdb760efb37ba9d84
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-09-04 12:36:07 +0530

    Exception test case commited

---
+++ AssignmentB/AssignmentB.Tests/MarklupTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AssignmentB.Tests
+{
+    [TestClass]
+    public class MarklupTest
+    {
+        [TestMethod]
+        public void AllSameData()
+        {
+            Itinerary publishedRateObject = new Itinerary();
+            publishedRateObject.BaseFareInUSD = 100;
+
+            List<Itinerary> discountedRates = new List<Itinerary>();
+            Itinerary discountedOne = new Itinerary();
+            discountedOne.BaseFareInUSD = 75;
+            discountedRates.Add(discountedOne);
+
+            Itinerary discountedTwo = new Itinerary();
+
+            discountedTwo.BaseFareInUSD = 75;
+            discountedRates.Add(discountedTwo);
+
+            List<Itinerary> markupAnswer = new List<Itinerary>();
+            MarkupManager markupManager = new MarkupManager();
+            markupAnswer = markupManager.CalculateMarkup(publishedRateObject, discountedRates);
+
+
+            decimal[] expected = { 15, 15 };
+            decimal[] actual = new decimal[2];
+            int i = 0;
+            foreach (Itinerary dis in markupAnswer)
+            {
+                actual[i] = dis.MarkupInUSD;
+                i++;
+            }
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void AllDifferentData()
+        {
+            Itinerary publishedRateObject = new Itinerary();
+            publishedRateObject.BaseFareInUSD = 100;
+            publishedRateObject.NumberOfStops = 3;
+
+            List<Itinerary> discountedRates = new List<Itinerary>();
+            Itinerary discountedOne = new Itinerary();
+            discountedOne.BaseFareInUSD = 75;
+            discountedOne.NumberOfStops = 2;
+            discountedRates.Add(discountedOne);
+
+
+            Itinerary discountedTwo = new Itinerary();
+
+            discountedTwo.BaseFareInUSD = 85;
+            discountedTwo.NumberOfStops = 4;
+            discountedRates.Add(discountedTwo);
+
+            List<Itinerary> markupAnswer = new List<Itinerary>();
+            MarkupManager markupManager = new MarkupManager();
+            markupAnswer = markupManager.CalculateMarkup(publishedRateObject, discountedRates);
+
+            decimal[] answer = { 35, 5 };
+            decimal[] answer2 = new decimal[2];
+            int i = 0;
+            foreach (Itinerary dis in markupAnswer)
+            {
+                answer2[i] = dis.MarkupInUSD;
+                i++;
+            }
+            CollectionAssert.AreEqual(answer, answer2);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ExceptionOccured()
+        {
+            Itinerary publishedRateObject=null;
+            List<Itinerary> discountedRates = new List<Itinerary>();
+            Itinerary discountedOne = new Itinerary();
+            discountedOne.BaseFareInUSD = 75;
+            discountedOne.NumberOfStops = 2;
+            discountedRates.Add(discountedOne);
+
+
+            Itinerary discountedTwo = new Itinerary();
+
+            discountedTwo.BaseFareInUSD = 85;
+            discountedTwo.NumberOfStops = 4;
+            discountedRates.Add(discountedTwo);
+
+            MarkupManager markupManager = new MarkupManager();
+            var markupAnswer = markupManager.CalculateMarkup(publishedRateObject, discountedRates);
+        }
+    }
+}
--- AssignmentB/AssignmentB/MarkupManager.cs
+++ AssignmentB/AssignmentB/MarkupManager.cs
@@ -10,36 +10,45 @@ namespace AssignmentB
     {
         public List<Itinerary> CalculateMarkup(Itinerary published, List<Itinerary> discounted)
         {
-             List<Itinerary> markupAnswer = new List<Itinerary>();
-            foreach (Itinerary dis in discounted)
+            if (published.Equals(null))
+                throw new NullReferenceException();
+            
+            List<Itinerary> markupAnswer = new List<Itinerary>();
+            foreach (Itinerary discount in discounted)
             {
-                if (IsAllDataSame(published, dis))
-                    markupAnswer.Add(CalculateMarkupWithSameData(published, dis));
-                else if (published.NumberOfStops > dis.NumberOfStops)
-                    markupAnswer.Add(CalculateMarkupWithlessNoOfStops(published, dis));
-                else if (published.NumberOfStops < dis.NumberOfStops)
-                    markupAnswer.Add(CalculateMarkupWithMoreNoOfStops(published, dis));
+                if (IsAllDataSame(published, discount))
+                    markupAnswer.Add(CalculateMarkupWithSameData(published, discount));
+
+                else if (published.NumberOfStops > discount.NumberOfStops)
+                    markupAnswer.Add(CalculateMarkupWithlessNoOfStops(published, discount));
+
+                else if (published.NumberOfStops < discount.NumberOfStops)
+                    markupAnswer.Add(CalculateMarkupWithMoreNoOfStops(published, discount));
             }
             return markupAnswer;
         }
+
         public bool IsAllDataSame(Itinerary published,Itinerary discounted)
         {
             if(published.NumberOfStops==discounted.NumberOfStops)
                 return true;
             return false;
         }
+
         public Itinerary CalculateMarkupWithSameData(Itinerary published, Itinerary discounted)
         {
             if ((published.BaseFareInUSD - discounted.BaseFareInUSD) > 15)
                 discounted.MarkupInUSD = published.BaseFareInUSD - discounted.BaseFareInUSD - 10;
             return discounted;
         }
+
         public Itinerary CalculateMarkupWithlessNoOfStops(Itinerary published, Itinerary discounted)
         {
             
                 discounted.MarkupInUSD = published.BaseFareInUSD - discounted.BaseFareInUSD + 10;
             return discounted;
         }
+
         public Itinerary CalculateMarkupWithMoreNoOfStops(Itinerary published, Itinerary discounted)
         {
                 discounted.MarkupInUSD = published.BaseFareInUSD - discounted.BaseFareInUSD - 10;
